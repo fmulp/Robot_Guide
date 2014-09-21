5576ec5c0f15319af9e4ee7f21aa75733933c90d
Andrei Dunets <dunets@gmail.com>
2014-09-21 10:15:28 +0300

    Cleanup code

--- kivyapp/main.py
+++ kivyapp/main.py
@@ -26,77 +26,73 @@ SPEED = 90
 
 
 class RobotGuideApp(App):
-    
-    sound = ObjectProperty(None, allownone=True)
+	
+	sound = ObjectProperty(None, allownone=True)
 
-    @property
-    def xml(self):
-        if not hasattr(self, '_xml'):
-            self._xml = parsexml.load_xml(XMLFILEPATH)
-        return self._xml
-        
-    @xml.setter
-    def xml(self, path):
-        self._xml = parsexml.load_xml(path) 
+	@property
+	def xml(self):
+		if not hasattr(self, '_xml'):
+			self._xml = parsexml.load_xml(XMLFILEPATH)
+		return self._xml
+		
+	@xml.setter
+	def xml(self, path):
+		self._xml = parsexml.load_xml(path) 
 
-    @property
-    def exhibits(self, index=0, name=None):
-        self._exhibits = parsexml.get_exhibits(parsexml.get_excursion(self.xml, index=index, name=name))
-        return self._exhibits
+	@property
+	def exhibits(self, index=0, name=None):
+		self._exhibits = parsexml.get_exhibits(parsexml.get_excursion(self.xml, index=index, name=name))
+		return self._exhibits
 
-    @exhibits.setter
-    def exhibits(self, value):
-        self._exhibits = value
-    @property
+	@exhibits.setter
+	def exhibits(self, value):
+		self._exhibits = value
+	@property
 
-    def currentindex(self):
-        if not hasattr(self, '_currentindex'):
-            self._currentindex = 0
-        return self._currentindex
+	def currentindex(self):
+		if not hasattr(self, '_currentindex'):
+			self._currentindex = 0
+		return self._currentindex
 
-    @currentindex.setter
-    def currentindex(self, value):
-        self._currentindex = value
+	@currentindex.setter
+	def currentindex(self, value):
+		self._currentindex = value
 
-    def show(self, *widgets):
-        global gRoot        
-        gRoot = self.root
-        for wdg in self.root.children[:]:
-            self.root.remove_widget(wdg)
-        for widget in widgets:
-            self.root.add_widget(widget)
-        if not widgets:
-            self.root.add_widget(self.root.ids.MainButton)
-            self.root.add_widget(self.root.ids.Logo, index=1)
+	def show(self, *widgets):
+		self.root.clear_widgets()
+		if not widgets:
+			widgets = self.root.ids
+		for widget in widgets:
+			self.root.add_widget(widget)
 
-    def scheduler(self, dt=0):
-        if self.currentindex < len(self.exhibits) - 2:
-            self.currentindex += 1
-            Clock.schedule_once(self.move_to_exhibit, 0)
-        else:
-            self.show()
-            self.root.ids.MainButton.text = 'Start over'
 
-    def move_to_exhibit(self, dt=0, index=None):
-        global gRoot
-        gRoot = self.root
-        if index is None:
-            index = self.currentindex
+	def scheduler(self, dt=0):
+		if self.currentindex < len(self.exhibits) - 2:
+			self.currentindex += 1
+			Clock.schedule_once(self.move_to_exhibit, 0)
+		else:
+			self.show()
+			self.root.ids.MainButton.text = 'Start over'
 
-        data = self.exhibits[index]
-        self.show(
-                Label(text=data['description'], font_size=self.root.ids.MainButton.font_size), 
-                Image(source=data['visualurl'])
-            )
+	def move_to_exhibit(self, dt=0, index=None):
+		if index is None:
+			index = self.currentindex
 
-        reqstring = HOST + '?' + str(data['x']) + '&' + str(data['y']) + '&' + str(data['phi'])
-        req = UrlRequest(reqstring)
+		data = self.exhibits[index]
+		label = Label(text=data['description'], font_size=self.root.ids.MainButton.font_size)
+		label.size_hint_y = 0.3
+		image = Image(source=data['visualurl'], scale=2)
+		image.allow_stretch = True
+		self.show(label, image)
 
-        sleep((data['x'] ** 2 + data['y'] ** 2) ** 0.5 / float(SPEED))
-        self.sound = SoundLoader.load(data['audiourl'])
-        self.sound.play()           
-        Clock.schedule_once(self.scheduler, self.sound.length + 7)
+		reqstring = HOST + '?' + data['x'] + '&' + data['y'] + '&' + data['phi']
+		req = UrlRequest(reqstring)
+
+		sleep((data['x'] ** 2 + data['y'] ** 2) ** 0.5 / float(SPEED))
+		self.sound = SoundLoader.load(data['audiourl'])
+		self.sound.play() 
+		Clock.schedule_once(self.scheduler, self.sound.length + 7)
 
 
 if __name__ == '__main__':
-    RobotGuideApp().run()
+	RobotGuideApp().run()
